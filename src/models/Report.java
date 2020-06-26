47c9fe72d30f5d17f991d7b61b513c9c3aa505ae
shintarou <saeki0816@gmail.com>
2020-06-26 22:13:47 +0900

    Modify top page

--- src/models/Report.java
+++ src/models/Report.java
@@ -16,16 +16,26 @@ import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Table(name = "reports")
-@NamedQueries({
-    @NamedQuery(
-            name = "getAllReports",
-            query = "SELECT r FROM Report AS r ORDER BY r.id DESC"
-            ),
-    @NamedQuery(
-            name = "getReportsCount",
-            query = "SELECT COUNT(r) FROM Report AS r"
-            ),
-})
+
+   @NamedQueries({
+                @NamedQuery(
+                        name = "getAllReports",
+                        query = "SELECT r FROM Report AS r ORDER BY r.id DESC"
+                        ),
+                @NamedQuery(
+                        name = "getReportsCount",
+                        query = "SELECT COUNT(r) FROM Report AS r"
+                        ),
+                @NamedQuery(
+                        name = "getMyAllReports",
+                        query = "SELECT r FROM Report AS r WHERE r.employee = :employee ORDER BY r.id DESC"
+                        ),
+                @NamedQuery(
+                        name = "getMyReportsCount",
+                        query = "SELECT COUNT(r) FROM Report AS r WHERE r.employee = :employee"
+                        )
+            })
+
 @Entity
 public class Report {
     @Id
@@ -106,6 +116,7 @@ public class Report {
     }
 
     public void setUpdated_at(Timestamp updated_at) {
-        this.updated_at = updated_at;
-    }
+        this.updated_at = updated_at;}
+
 }
+
